a081e598c60d0bfc08e53f8cc584fd78eae7c48a
cBombs <richie.ucsb@gmail.com>
2023-06-15 20:24:15 -0700

    file structure setup, client, smart-contract

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,45 +1,46 @@
 /*
  * Author: CastleBomber
  * Date: June 15th, 2023
- * 
- * 
+ *
  * Tutorial: Build and Deploy a Modern Web 3.0 Blockchain App | Solidity, Smart Contracts, Crypto
  * Link: https://www.youtube.com/watch?v=Wn_Kb3MR_cU&ab_channel=JavaScriptMastery
- * 
-*/
+ *
+ *
+ * Shortcuts:
+ *  VS Code:
+ * 	    c++ VS Code clang-formatter: shift+alt+f
+ *      Go to definition - F12
+ *
+ *  Visual Studio:
+ *      code folding: select region, ctrl+m+m
+ *      full screen: shift+alt+enter
+ *      solution explorer: ctrl+alt+L
+ *      (start debugger to access watchlist)
+ *      watchlist: ctrl+alt+W,1
+ *      add to watchlist: shift+F9
+ *		  terminal: ctrl + `
+ */
 
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {
+  Navbar,
+  Welcome,
+  Footer,
+  Services,
+  Transactions,
+} from "./components/index";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="min-h-screen">
+      <div className="gradient-bg-welcom">
+        <Navbar />
+        <Welcome />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+      <Services />
+      <Transactions />
+      <Footer />
+    </div>
+  );
+};
 
-export default App
+export default App;
